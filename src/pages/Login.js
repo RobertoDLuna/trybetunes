8eefa515ca6f1b1c6c486f2d1dbbee8f2545561f
Roberto Luna <robertocgw@gmail.com>
2022-05-18 19:08:32 -0300

    função de validação de login, ainda sem funcionar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
   constructor() {
@@ -7,14 +8,25 @@ class Login extends React.Component {
     state = {
       name: '',
       disabledButton: true,
-      loading: '',
+      loadingMessage: true,
     };
     this.props = this.onInputChange.bind(this);
   }
 
   // Salva as alterações no estado
   onInputChange({ target }) {
-    this.setState(() => ({ [target.name]: target.value }));
+    this.setState(() => ({ [target.name]: target.value }), this.validateLogin);
+  }
+
+  // Verifica se o login é válido
+  validateLogin() {
+    const { name } = this.state;
+    const validLength = 3;
+		if (name.length >= validLength) {
+			this.setState( disabledButton:true )
+		} else {
+			this.setState( disabledButton:false )
+		}
   }
 
   render() {
@@ -24,6 +36,7 @@ class Login extends React.Component {
           data-testid="login-name-input"
           type="text"
           name="name"
+          onChange={ this.onInputChange }
         />
         <button
           data-testid="login-submit-button"
